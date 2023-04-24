d454588c15d7e36d4926426dafa43d2bed993286
Beksultan Almazbekov <anime.beksultan@gmail.com>
2023-04-24 10:34:56 +0600

    update

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -1,8 +1,35 @@
-
 #include <iostream>
 #include <fstream>
 
 using namespace std;
-main() {
+int main() {
+	ifstream fin("in.txt");
+	ofstream fout("out.txt");
+
+	int n, i, m;
+	fin >> n;
+	int* arr1 = new int[n];
+	for (i = 0; i < n; ++i) {
+		fin >> arr1[i];
+	}
+	fin >> m;
+	int* arr2 = new int[m];
+	for (i = 0; i < m; ++i) {
+		fin >> arr2[i];
+	}
+	for (i = 1; i < n; ++i) {
+		swap(arr1[i], arr1[i - 1]);
+	}
+	for (i = m-1; i > 0; --i) {
+		swap(arr2[i], arr2[i - 1]);
+	}
+	for (i = 0; i < m; ++i) {
+		fout << arr2[i] << " ";
+	}
+	fout << endl;
+	for (i = 0; i < n; ++i) {
+		fout << arr1[i] << " ";
+	}
+
 
 }
